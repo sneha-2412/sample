1bf9c9fc84542e02ce5e694ccb7491304db3c75b
91888 <91888@GM>
2025-08-10 11:15:40 +0530

    Second Commit

--- BootProj/src/main/java/nit/BootProj/App.java
+++ BootProj/src/main/java/nit/BootProj/App.java
@@ -5,6 +5,6 @@ package nit.BootProj;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello everyone World!");
     }
 }
